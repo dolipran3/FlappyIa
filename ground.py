40e79ede186d73ddb5d7bfb18a269158dc4043e2
baptiste <baptiste.le-bail@proton.me>
2026-03-06 20:17:25 +0100

    fix: clean up ground initialization and ensure proper formatting

--- ground.py
+++ ground.py
@@ -6,6 +6,7 @@ class Ground:
     def __init__(self):
         self.image = pygame.image.load("Img/base.png").convert()
         self.rect = self.image.get_rect()
+        self.rectTop = pygame.Rect(0, -200, self.rect.width, 5) #TODO: A revoir, pas très propre.
         self.groundScroll = 0
         self.groundSpeed = 2
         self.rect.topleft = (self.groundScroll, HEIGHT - (self.rect.height// 1.3))
@@ -21,4 +22,3 @@ class Ground:
 
     def draw(self, screen):
         screen.blit(self.image, self.rect)
-    
